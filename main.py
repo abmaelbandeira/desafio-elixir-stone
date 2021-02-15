302c7fcb97bdb382df406b3fdf090385f96636bd
Abmael Bandeira <abmaelbandeira@hotmail.com>
2021-02-15 09:18:09 -0300

    Refactory main.py

--- main.py
+++ main.py
@@ -2,21 +2,6 @@ from shopping_list.shopping_list import shopping_list_func
 
 
 if __name__ == '__main__':
-    shopping_list = []
-    email_list = []
-    print('=====Shopping list====')
-    while True:
-        product = input("Product: ")
-        if product == "end":
-            break
-        amount = int(input("Amount: "))
-        price = int(input("Price: "))
-        shopping_list.append([product, amount, price])
-
-    print("====Emails list====")
-    while True:
-        email = input("Email: ")
-        if email == "end":
-            break
-        email_list.append(email)
+    shopping_list = [['a', 1, 6], ['b', 1, 6], ['c', 1, 5]]
+    email_list = ['a', 'b', 'c', 'd', 'e']
     print(shopping_list_func(shopping_list, email_list))
